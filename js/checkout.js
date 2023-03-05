d6f16c722ebe3e2083ff66c93beda54961953f31
Ali Janbein <alijanbein999@gmail.com>
2023-03-05 21:21:48 +0200

    added api to chechout

--- js/checkout.js
+++ js/checkout.js
@@ -1,40 +1,69 @@
-let is_user = false;
+let is_user = true;
 let is_billing_accepted = false;
-let pay_method  = false;
-
-window.onload = ()=> {
-    console.log("init");
-}
+let pay_method = false;
+let id = "";
+let method = "";
 
 // if (sessionStorage.getItem("userId")){
-//     is_billing_accepted = true;
-//     let id = sessionStorage.getItem("userId");
+//     is_user = true;
+//     id = sessionStorage.getItem("userId");
+//     testToShow(billing, is_user);
 // }
 const billing = document.getElementById("billing_content");
 const payment = document.getElementById("payment_content");
 const submit_button = document.getElementById("submit");
 const login = document.getElementById("login");
 const register = document.getElementById("register");
+const accept = document.getElementById("accept");
+const delivery = document.getElementById("delivery");
+const visa = document.getElementById("visa");
 
-login.addEventListener("click",()=>{
-    window.location.href = "./login.html";
-})
-register.addEventListener("click",()=>{
-    window.location.href = "./register.html";
-})
+login.addEventListener("click", () => {
+  window.location.href = "./login.html";
+});
+register.addEventListener("click", () => {
+  window.location.href = "./register.html";
+});
+accept.addEventListener("click", () => {
+  is_billing_accepted = true;
+  testToShow(payment, is_billing_accepted);
+});
+delivery.addEventListener("click", () => {
+  method = "delivery";
+  pay_method = true;
+  testToShow(submit_button, pay_method);
+});
+visa.addEventListener("visa", () => {
+  method = "visa";
+  pay_method = true;
+  testToShow(submit_button, pay_method);
+});
 
-const testToShow = (element,permition) => {
-    console.log(permition);
-    if(permition){
-        element.style.display = "block";
-        element.style.transition = "1ms";
-    }
-    else {
-        element.style.display = "none";
-    }
-}
+submit_button.addEventListener("click", () => {
+  const data = new FormData();
+  data.append("user_id", 1);
+  data.append("cart_id", 1);
+  data.append("method",method);
+  axios
+    .post("http://localhost//ecommerce-backend/chechout_api.php", data)
+    .then(function (res) {
+      window.location.href = "../index.html";
+      console.log(res.data.result);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
 
-testToShow(billing,is_user);
-testToShow(payment,is_billing_accepted);
-testToShow(submit_button,pay_method);
+const testToShow = (element, permition) => {
+  if (permition) {
+    element.style.display = "block";
+    element.style.transition = "1ms";
+  } else {
+    element.style.display = "none";
+  }
+};
 
+testToShow(billing, is_user);
+testToShow(payment, is_billing_accepted);
+testToShow(submit_button, pay_method);
