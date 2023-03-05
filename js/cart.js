dd0dfd065c63f737dbbef9b07b58e5283292334b
Ali Janbein <alijanbein999@gmail.com>
2023-03-05 19:08:49 +0200

    added card_api comlete

--- js/cart.js
+++ js/cart.js
@@ -13,29 +13,38 @@ const array = [
     },
 
 ];
+window.onload = () => {
+   axios.get("http://localhost//ecommerce-backend/cart_api.php").then((res)=>{
+        const DATA = res.data;
+        console.log(DATA);
 
-const cart = document.getElementsByClassName("check__cart")[0];
-console.log(cart);
-array.map(data => {
-    const product = document.createElement("div");
-    const hr =document.createElement("hr");
-    product.innerHTML = `<div class="check__product">
-    <img src=${data.image} alt="" />
-    <div class="check__product__info">
-        <h5>
-            ${data.name}
-        </h5>
-      <p>
-      ${data.description}
-      </p>
-      <div class="buttons">
-        <input id="in1" type="number" value="1">
-        <button class="update">update</button>
-        <button class="delete">x</button>
-      </div>
-      <h6>$${data.price}</h6>
-    </div>`
-    cart.appendChild(hr)
-    cart.appendChild(product);
-    
-})
+        const cart = document.getElementsByClassName("check__cart")[0];
+        console.log(cart);
+        DATA.map((data) => {
+          const product = document.createElement("div");
+          const hr = document.createElement("hr");
+          product.innerHTML = `<div class="check__product">
+          <img src=${data.img} alt="" />
+          <div class="check__product__info">
+              <h5>
+                  ${data.name}
+              </h5>
+            <p>
+            ${data.desc}
+            </p>
+            <div class="buttons">
+              <input id="in1" type="number" value="1">
+              <button class="update">update</button>
+              <button class="delete">x</button>
+            </div>
+            <h6>$${data.price}</h6>
+          </div>`;
+          cart.appendChild(hr);
+          cart.appendChild(product);
+        });
+
+   }).catch((err) => {
+    console.error(err)
+});
+  
+};
